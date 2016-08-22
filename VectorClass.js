00249677961e48ac89ef90ce6e88df503a2dc135
JosafatV <josav09@gmail.com>
2016-08-22 13:46:29 -0600

    vectorClassv1

---
+++ VectorClass.js
@@ -0,0 +1,16 @@
+/*Vector dfinition */
+class Vector {
+  constructor(x, y, z) {
+    this.x=x;
+    this.y=y;
+    this.z=z;
+  }
+
+  magitudAlCuadrado(){
+    return this.x*this.x+this.y*this.y+this.z*this.z
+  }
+
+  Cuadrado(num) {
+    num
+  }
+}
