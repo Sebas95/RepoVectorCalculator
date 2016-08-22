fd8837ba3f4b39810c4d8adefdb9c45f99587ec5
JosafatV <josav09@gmail.com>
2016-08-22 14:17:08 -0600

    first instance of Vops

---
+++ VectorOps.js
@@ -0,0 +1,30 @@
+/*Vector dfinition */
+class VectorOps {
+  constructor() {
+  }
+
+    sumarVectores(VectorA, VectorB){
+      var VectorC = new Vector (VectorA.x+VectorB.x, VectorA.y+VectorB.y, VectorA.z+VectorB.z)
+      return VectorC
+    }
+
+    restarVectores(VectorA, VectorB){
+      var VectorC = new Vector (VectorA.x-VectorB.x, VectorA.y-VectorB.y, VectorA.z-VectorB.z)
+      return VectorC
+    }
+
+    puntoVectores(VectorA, VectorB){
+      var Producto = new Vector (VectorA.x*VectorB.x+VectorA.y*VectorB.y+VectorA.z*VectorB.z)
+      return Producto
+    }
+
+    cruzVectores(VectorA, VectorB){
+      var VectorC = new Vector (VectorA.y*VectorB.z-VectorA.z*VectorB.y,
+                                VectorA.x*VectorB.z-VectorA.z*VectorB.x,
+                                VectorA.x*VectorB.y-VectorA.y*VectorB.x)
+      return VectorC
+    }
+
+
+
+}
